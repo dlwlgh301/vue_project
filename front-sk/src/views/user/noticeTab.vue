e2cc1184d6df60ee3d153d4b641ace5ec9a1a093
임현수 <21port@naver.com>
2020-02-05 17:23:03 +0900

    fix pageTitle error issue:S02P13D106-89 date:200205

--- front-sk/src/views/user/noticeTab.vue
+++ front-sk/src/views/user/noticeTab.vue
@@ -29,7 +29,7 @@
                         <template v-for="(follow_item, index) in follow_items">
                             <v-subheader v-if="follow_item.header" :key="follow_item.header">{{ follow_item.header }}</v-subheader>
                             <v-divider v-else-if="follow_item.divider" :inset="follow_item.inset" :key="index"></v-divider>
-                            <v-list-item v-else :key="index" avatar v-show="!follow_item.accept">
+                            <v-list-item v-else :key="follow_item.userId" avatar v-show="!follow_item.accept">
                                 <v-list-item-avatar>
                                     <img :src="follow_item.avatar" style="width: 2rem; height: 2rem; border-radius:50%" />
                                 </v-list-item-avatar>
@@ -37,8 +37,10 @@
                                     <v-list-item-title v-html="follow_item.userId"></v-list-item-title>
                                     <v-list-item-subtitle v-html="follow_item.subtitle"></v-list-item-subtitle>
                                 </v-list-item-content>
-                                <v-btn class="btn-accept" small max-width="3rem" style="position:relative" @click="newFollow()">요청 수락</v-btn>
-                                <v-btn text icon color="#fff" @click="removeFollow(index)">
+                                <v-btn class="btn-accept" small max-width="3rem" style="position:relative" @click="followAccept" v-show="true"
+                                    >수락</v-btn
+                                >
+                                <v-btn text icon color="#fff">
                                     <v-icon class="btn-delete" size="0.8rem">mdi-trash-can-outline</v-icon>
                                 </v-btn>
                             </v-list-item>
@@ -56,34 +58,17 @@ import UserApi from '../../apis/UserApi';
 
 export default {
     name: 'noticeTab',
+    created() {
+        this.$store.commit('setPageTitle', '알림');
+    },
     data() {
         return {
+            pageTitle: '알림',
             email: 'ihs3583@gmail.com',
-            isremove: [],
             tab: null,
             tab_names: ['알림', '팔로우 요청'],
-            notice_items: [{ header: '알림' }],
-            follow_items: [
-                { header: '팔로우 요청' },
-                {
-                    avatar:
-                        'https://i.guim.co.uk/img/media/88f6b98714035656cb18fb282507b60e82edb0d7/0_35_2560_1536/master/2560.jpg?width=300&quality=85&auto=format&fit=max&s=6dc12c01b7d052a59201b5e2b4697ff1',
-                    userId: '이지호',
-                    subtitle: '팔로우 요청을 보냈습니다'
-                },
-                { divider: true, inset: true },
-                {
-                    avatar: '/static/doc-images/lists/2.jpg',
-                    title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-                    subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-                },
-                { divider: true, inset: true },
-                {
-                    avatar: '/static/doc-images/lists/3.jpg',
-                    title: 'Oui oui',
-                    subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-                }
-            ]
+            notice_items: [],
+            follow_items: []
         };
     },
     methods: {
@@ -112,27 +97,12 @@ export default {
                 }
             );
         },
-        newFollow: function() {
-            this.follow_items.push({
-                avatar:
-                    'https://i.guim.co.uk/img/media/88f6b98714035656cb18fb282507b60e82edb0d7/0_35_2560_1536/master/2560.jpg?width=300&quality=85&auto=format&fit=max&s=6dc12c01b7d052a59201b5e2b4697ff1',
-                userId: '이지호',
-                subtitle: '팔로우 요청을 또 보냈습니다'
-            });
-            this.follow_items.push({ divider: true, inset: true });
-        },
-        removeFollow(idx) {
-            console.log(this.follow_items);
-            console.log(idx);
-            this.isremove.push(idx);
-            this.follow_items.splice(idx);
-            console.log('isremove', this.isremove);
-            // this.follow_items.splice(idx);
+        followAccept: function() {
+            this.isAccept = false;
         }
     },
     mounted() {
         this.loadNotice();
-        this.$store.commit('setPageTitle', '알림');
     }
 };
 </script>
