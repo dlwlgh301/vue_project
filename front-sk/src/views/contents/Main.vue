71c63ea56743c9eefed8dffa8a329e7a01582265
김민정 <pacedov3@gmail.com>
2020-02-13 15:46:02 +0900

    [modify] 알람 백&포그라운드 수정 완료(둘 다 되는 코드)

--- front-sk/src/views/contents/Main.vue
+++ front-sk/src/views/contents/Main.vue
@@ -11,7 +11,11 @@
                         <span id="pimg">img</span>
                         <span id="pname">name</span>
                     </div>
-                    <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png" style="margin-bottom:0.5rem"></v-img>
+                    <v-img
+                        height="250"
+                        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                        style="margin-bottom:0.5rem"
+                    ></v-img>
                     <md-button class="md-icon-button">
                         <md-icon>favorite_border</md-icon>
                     </md-button>
@@ -19,11 +23,21 @@
                         <md-icon>chat_bubble_outline</md-icon>
                     </md-button>
 
-                    <v-card-title style="line-height: 0.5rem" v-model="title"></v-card-title>
+                    <v-card-title
+                        style="line-height: 0.5rem"
+                        v-model="title"
+                    ></v-card-title>
 
                     <v-card-text>
                         <v-row align="center" class="mx-0">
-                            <v-rating :value="rating" color="amber" dense half-increments readonly size="14"></v-rating>
+                            <v-rating
+                                :value="rating"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
 
                             <div class="grey--text ml-4" v-bind="rating"></div>
                         </v-row>
@@ -35,29 +49,52 @@
                 </v-card>
             </v-col>
             <v-col cols="12" md="4">
-                <v-card :loading="loading" class="pa-2" max-width="374" :elevation="4">
+                <v-card
+                    :loading="loading"
+                    class="pa-2"
+                    max-width="374"
+                    :elevation="4"
+                >
                     <div style="padding: 14px;">
                         <span id="pimg">img</span>
                         <span id="pname">name</span>
                     </div>
-                    <v-img height="250" src="https://file.mk.co.kr/mkde_7/N0/2019/09/20190911_4227416_1568180445.jpg"></v-img>
+                    <v-img
+                        height="250"
+                        src="https://file.mk.co.kr/mkde_7/N0/2019/09/20190911_4227416_1568180445.jpg"
+                    ></v-img>
                     <md-button class="md-icon-button">
                         <md-icon>favorite_border</md-icon>
                     </md-button>
                     <md-button class="md-icon-button">
                         <md-icon>chat_bubble_outline</md-icon>
                     </md-button>
-                    <v-card-title style="line-height: 0.5rem">스타벅스</v-card-title>
+                    <v-card-title style="line-height: 0.5rem"
+                        >스타벅스</v-card-title
+                    >
 
                     <v-card-text>
                         <v-row align="center" class="mx-0">
-                            <v-rating :value="3.5" color="amber" dense half-increments readonly size="14"></v-rating>
+                            <v-rating
+                                :value="3.5"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
 
                             <div class="grey--text ml-4">3.5 (212)</div>
                         </v-row>
-                        <div class="my-4 subtitle-1 black--text">어딘가의 커피</div>
+                        <div class="my-4 subtitle-1 black--text">
+                            어딘가의 커피
+                        </div>
 
-                        <div>커피가 밥값보다 비쌈 근데 커피는 맛있음 근데 밥값보다 비쌈 근데 커피는 괜찮음..근데 그돈이면 국밥사먹음</div>
+                        <div>
+                            커피가 밥값보다 비쌈 근데 커피는 맛있음 근데
+                            밥값보다 비쌈 근데 커피는 괜찮음..근데 그돈이면
+                            국밥사먹음
+                        </div>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -67,7 +104,11 @@
                         <span id="pimg">img</span>
                         <span id="pname">name</span>
                     </div>
-                    <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png" style="margin-bottom:0.5rem"></v-img>
+                    <v-img
+                        height="250"
+                        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                        style="margin-bottom:0.5rem"
+                    ></v-img>
                     <md-button class="md-icon-button">
                         <md-icon>favorite_border</md-icon>
                     </md-button>
@@ -75,11 +116,21 @@
                         <md-icon>chat_bubble_outline</md-icon>
                     </md-button>
 
-                    <v-card-title style="line-height: 0.5rem" v-model="title"></v-card-title>
+                    <v-card-title
+                        style="line-height: 0.5rem"
+                        v-model="title"
+                    ></v-card-title>
 
                     <v-card-text>
                         <v-row align="center" class="mx-0">
-                            <v-rating :value="rating" color="amber" dense half-increments readonly size="14"></v-rating>
+                            <v-rating
+                                :value="rating"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
 
                             <div class="grey--text ml-4" v-bind="rating"></div>
                         </v-row>
@@ -91,29 +142,52 @@
                 </v-card>
             </v-col>
             <v-col cols="12" md="4">
-                <v-card :loading="loading" class="pa-2" max-width="374" :elevation="4">
+                <v-card
+                    :loading="loading"
+                    class="pa-2"
+                    max-width="374"
+                    :elevation="4"
+                >
                     <div style="padding: 14px;">
                         <span id="pimg">img</span>
                         <span id="pname">name</span>
                     </div>
-                    <v-img height="250" src="https://file.mk.co.kr/mkde_7/N0/2019/09/20190911_4227416_1568180445.jpg"></v-img>
+                    <v-img
+                        height="250"
+                        src="https://file.mk.co.kr/mkde_7/N0/2019/09/20190911_4227416_1568180445.jpg"
+                    ></v-img>
                     <md-button class="md-icon-button">
                         <md-icon>favorite_border</md-icon>
                     </md-button>
                     <md-button class="md-icon-button">
                         <md-icon>chat_bubble_outline</md-icon>
                     </md-button>
-                    <v-card-title style="line-height: 0.5rem">스타벅스</v-card-title>
+                    <v-card-title style="line-height: 0.5rem"
+                        >스타벅스</v-card-title
+                    >
 
                     <v-card-text>
                         <v-row align="center" class="mx-0">
-                            <v-rating :value="3.5" color="amber" dense half-increments readonly size="14"></v-rating>
+                            <v-rating
+                                :value="3.5"
+                                color="amber"
+                                dense
+                                half-increments
+                                readonly
+                                size="14"
+                            ></v-rating>
 
                             <div class="grey--text ml-4">3.5 (212)</div>
                         </v-row>
-                        <div class="my-4 subtitle-1 black--text">어딘가의 커피</div>
+                        <div class="my-4 subtitle-1 black--text">
+                            어딘가의 커피
+                        </div>
 
-                        <div>커피가 밥값보다 비쌈 근데 커피는 맛있음 근데 밥값보다 비쌈 근데 커피는 괜찮음..근데 그돈이면 국밥사먹음</div>
+                        <div>
+                            커피가 밥값보다 비쌈 근데 커피는 맛있음 근데
+                            밥값보다 비쌈 근데 커피는 괜찮음..근데 그돈이면
+                            국밥사먹음
+                        </div>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -150,12 +224,13 @@ export default {
         return {
             title: '',
             content: '',
-            rating: '',
+            rating: 0,
             keyword1: '',
             photo: '',
             keyword2: '',
             like: '',
-            currentDate: new Date()
+            currentDate: new Date(),
+            loading: true
         };
     },
     methods: {
