d7916c290be0c9383c19e7d57b6d3d64573518fb
임현수 <21port@naver.com>
2020-02-14 13:36:27 +0900

    update url for mainPage

--- front-sk/src/views/user/Login.vue
+++ front-sk/src/views/user/Login.vue
@@ -101,7 +101,7 @@ export default {
     },
     created() {
         if (sessionStorage.getItem('email') != null) {
-            this.$router.push('/main');
+            this.$router.push('/contents/main');
         }
         this.component = this;
         this.$store.commit('setPageTitle', '로그인');
@@ -209,7 +209,7 @@ export default {
                                 msg: email + '님이 로그인하였습니다.',
                                 receiver: email
                             });
-                            this.$router.push('/main');
+                            this.$router.push('/contents/main');
                         }
                     },
                     error => {
